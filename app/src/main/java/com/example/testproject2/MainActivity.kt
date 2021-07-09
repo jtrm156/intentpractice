f3e5e269014d61d75b6b3e7c02415f21d4d59d87
blanco <skrk309@naver.com>
2021-07-10 06:56:21 +0900

    recyclerview success

--- app/src/main/java/com/example/testproject2/MainActivity.kt
+++ app/src/main/java/com/example/testproject2/MainActivity.kt
@@ -16,14 +16,14 @@ class MainActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-        binding.btnMain.setOnClickListener{
+        binding.btn1Main.setOnClickListener{
 
             var intent = Intent(this,SecondActivity::class.java)
             intent.putExtra("data",binding.edtMain.text.toString())
             startActivity(intent)
         }
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show()
     } //액티비티가 만들어질때 생성되는 메소드
       //메인 액티비티에 edittext에 값을 입력한 후 버튼을 클릭하면 입력한 값을 인텐트에 담아서 Second Actvitiy로 화면 전환을 해주고
       //Second Activity에 있는 textview에 받아온 값을 세팅
@@ -31,37 +31,37 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show()
     }
 
     override fun onResume() {
         super.onResume()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show()
     }
 
     override fun onPause() {
         super.onPause()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show()
     }
 
     override fun onStop() {
         super.onStop()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show()
     }
 
     override fun onRestart() {
         super.onRestart()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show()
         //actvitiy가 파괴되기 직전에 호출되는 메소드 이 메소드를 실행 후 액티비티는 파괴
     }
 }
--- app/src/main/java/com/example/testproject2/SecondActivity.kt
+++ app/src/main/java/com/example/testproject2/SecondActivity.kt
@@ -1,11 +1,23 @@
 package com.example.testproject2
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.homework2.CustomAdapter
 import com.example.testproject2.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    var account_array = arrayListOf<DataVo>(
+        DataVo("12345678912","입금:50000원","2021-07-10"),
+        DataVo("12345678912","출금:35200원","2021-07-02"),
+        DataVo("12345678912","입금:880000원","2021-07-01"),
+        DataVo("12345678912","출금:39000원","2021-06-30"),
+        DataVo("12345678912","출금:10000원","2021-06-28"),
+        DataVo("12345678912","출금:70000원","2021-06-25"),
+        DataVo("12345678912","출금:18000원","2021-06-20"),
+    )
 
     lateinit var binding:ActivitySecondBinding
 
@@ -15,6 +27,49 @@ class SecondActivity : AppCompatActivity() {
         val view =binding.root
         setContentView(view)
 
-        binding.txtSecond.text=intent.getStringExtra("data")
+        //binding.txtSecond.text=intent.getStringExtra("data")
+
+        val mAdapter = CustomAdapter(this,account_array)
+        binding.RCSub.adapter = mAdapter
+        val layout = LinearLayoutManager(this)
+        binding.RCSub.layoutManager = layout
+        binding.RCSub.setHasFixedSize(true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show()
+        //actvitiy가 파괴되기 직전에 호출되는 메소드 이 메소드를 실행 후 액티비티는 파괴
     }
 }
